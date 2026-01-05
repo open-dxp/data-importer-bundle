cd79e35040b870f06a101aa127b35a2ec119b109
open-dxp-stack <stack@opendxp.ch>
2026-01-05 09:44:27 +0100

    Update cs-fixer config

---
+++ .php-cs-fixer-finder.dist.php
@@ -0,0 +1,12 @@
+<?php
+
+return PhpCsFixer\Finder::create()
+    ->in([
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ])
+
+    ->exclude([
+        __DIR__ . '/tests/_output',
+        __DIR__ . '/tests/Support/_generated',
+]);
